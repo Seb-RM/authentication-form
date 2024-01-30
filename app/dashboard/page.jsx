617e3f20ccaf88e380e855ea59b1cab41aab36d9
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-01-30 17:48:18 -0300

    nav bar y clerk middleware

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <h1 className='text-2xl font-bold mb-5'>DashBoard</h1>
+            <p className='mb-5'>Welcome to the Dashboard</p>
+        </>
+    )
+}
+
+export default page
